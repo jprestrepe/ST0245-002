77d90261bd6cbaa0e723489e90b527b6f8febbfb
jjsanchezc <68928367+jjsanchezc@users.noreply.github.com>
2020-08-25 16:39:36 -0500

    Update Punto 1.1 & 1.2.java

--- laboratorios/lab01/codigo/Punto 1.1 & 1.2.java
+++ laboratorios/lab01/codigo/Punto 1.1 & 1.2.java
@@ -1,14 +1,21 @@
 //Punto 1.1
-    public class subSecuencia {
-    // Este metodo sirve para ingresar los datos para ver cual es la subsecuencia mas larga
-    
+
+  // Este metodo sirve para ingresar los datos para ver cual es la subsecuencia
+    // mas larga
     public static int SubSec1(String Sec1, String Sec2) {
-    return SubSec(Sec1.length(), Sec2.length(), Sec1, Sec2);
-   }  
+        return SubSec(Sec1.length(), Sec2.length(), Sec1, Sec2);
+    }
+
     public static int SubSec(int a, int b, String Sec1, String Sec2) {
-    return Math.max(SubSec(a, b - 1, Sec1, Sec2), SubSec(a - 1, b, Sec1, Sec2));
-   }
+        if (a == 0 || b == 0)
+            return 0;
+        if (Sec1.charAt(a - 1) == Sec2.charAt(b - 1))
+            return 1 + SubSec(a - 1, b - 1, Sec1, Sec2);
+        return Math.max(SubSec(a, b - 1, Sec1, Sec2), SubSec(a - 1, b, Sec1, Sec2));
+    }
 }
+
+
 
 //Punto 1.2
 public class subSecuencia {
