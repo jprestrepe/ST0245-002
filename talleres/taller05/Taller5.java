d7317d463c4c2d949fa9976be15b932582f2a20e
jjsanchezc <68928367+jjsanchezc@users.noreply.github.com>
2020-08-28 14:11:24 -0500

    Update Taller5.java

--- talleres/taller05/Taller5.java
+++ talleres/taller05/Taller5.java
@@ -21,7 +21,35 @@ package proyecto;
         }
     }
     
-     public static int[] insertionSort (int[] array){
-      
-    }       
+
+    public static int insertinSort(int[] array) {
+        return noSe(0, 1, array);
+    }
+
+    public static int noSe(int a, int b, int[] array) {
+        
+        
+        
+        if (a == array.length - 1) {
+            //el siguiente ciclo imprime el arreglo ya organizado
+            int i = 0;
+            while(i < array.length){
+            System.out.println(""+array[i]);
+            i++;
+            }
+            return 0;
+        }
+
+        int Aux = array[a];
+        if (array[b] < array[a]) {
+            array[a] = array[b];
+            array[b] = Aux;
+            if (a - 1 < 0) {
+                return noSe(a + 1, b + 1, array);
+            }
+            return noSe(a - 1, b - 1, array);
+        }
+        return noSe(a + 1, b + 1, array);
+    }
+        
  }
