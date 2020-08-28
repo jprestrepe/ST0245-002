9b3ca933063322fca8815a8c1d3e6b988d86e74d
jjsanchezc <68928367+jjsanchezc@users.noreply.github.com>
2020-08-28 14:13:51 -0500

    Update Taller5.java

--- talleres/taller05/Taller5.java
+++ talleres/taller05/Taller5.java
@@ -5,37 +5,37 @@ package proyecto;
  * @author Juan Pablo Restrepo, Juan José Sanchez, Mauricio Toro, Andres Paez
  */
  
- public class Taller5{
-     public static int suma (int[]array){ //O(n)
-     int resultado = 0; 
-        
-     for (int i = 0; i < array.length ; i++) {
-     resultado += array[i]; 
+public class Taller5 {
+    public static int suma(int[] array) { // O(n)
+        int resultado = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            resultado += array[i];
+        }
+        return resultado;
     }
-     return resultado; 
-    }
-    
-    public static void mul (int num){ //O(n)
-    for (int i = 0; i <= 10; i++) {
-    System.out.println(num + " x " + i + " = " + (num * i)); 
+
+    public static void mul(int num) { // O(n)
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
         }
     }
-    
 
+ 
+ 
+    // punto de insertion sort
     public static int insertinSort(int[] array) {
         return noSe(0, 1, array);
     }
 
     public static int noSe(int a, int b, int[] array) {
-        
-        
-        
+
         if (a == array.length - 1) {
-            //el siguiente ciclo imprime el arreglo ya organizado
+            // el siguiente ciclo imprime el arreglo ya organizado
             int i = 0;
-            while(i < array.length){
-            System.out.println(""+array[i]);
-            i++;
+            while (i < array.length) {
+                System.out.println("" + array[i]);
+                i++;
             }
             return 0;
         }
@@ -51,5 +51,5 @@ package proyecto;
         }
         return noSe(a + 1, b + 1, array);
     }
-        
- }
+
+}
